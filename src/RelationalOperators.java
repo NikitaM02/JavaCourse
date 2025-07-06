79f715304c51cbab64ba24e3ee892db19a93e006
Venu Gopal <nikitamanjare17@gmail.com>
2025-07-06 20:20:22 +0530

    RelationalOperators

---
+++ src/RelationalOperators.java
@@ -0,0 +1,8 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        boolean b1 = a > b;
+        System.out.println(b1);
+    }
+}
