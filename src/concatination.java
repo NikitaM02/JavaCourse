952a81e5f860d46e3a276a7c99ff5a14de02f587
Venu Gopal <nikitamanjare17@gmail.com>
2025-07-06 20:55:26 +0530

    LogicalOperators

---
+++ src/concatination.java
@@ -0,0 +1,11 @@
+public class concatination {
+    public static void main(String[] args) {
+        //concatination
+        System.out.println("Nikita" + "Venu");
+        //concatination
+        int a = 77;
+        int b = 22;
+        System.out.println("sum" + a+b);
+
+    }
+}
