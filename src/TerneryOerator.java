a766afe9a6a0c6113557609f313a49a80a442757
Venu Gopal <nikitamanjare17@gmail.com>
2025-07-08 23:21:42 +0530

    Ternary operator

---
+++ src/TerneryOerator.java
@@ -0,0 +1,7 @@
+public class TerneryOerator {
+    public static void main(String[] args) {
+        int num =13;
+        String result = (num%2 ==0) ? "Even" : "odd";
+        System.out.println(result);
+    }
+}
